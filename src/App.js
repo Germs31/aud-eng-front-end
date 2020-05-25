f3a59f31a177135b65e9dda791875c5b59ac175a
German Nunez <german90nunez@gmail.com>
2020-05-25 12:46:58 -0700

    added material ui and very simple form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Grid container spacing={3}>
+        <Grid item m={6}>
+          <h1>Aud-Eng</h1>
+          <h3>Find your solution</h3>
+        </Grid>
+        <Grid className="login-form" item m={6}>
+          <h4>Login</h4>
+          <form>
+            <TextField variant="outlined"
+                       placeholder="Username"
+                       type="text"></TextField>
+            <TextField variant="outlined"
+                       placeholder="Password"
+                       type="password"></TextField>
+            <Button variant="contained"
+                    color="#1D2228">Login</Button>
+          </form>
+        </Grid>
+      </Grid>
     </div>
   );
 }
