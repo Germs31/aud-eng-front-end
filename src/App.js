90c2ca2ffb33ab7e894638048f3700c6e031ab2f
German Nunez <german90nunez@gmail.com>
2020-05-27 15:38:50 -0700

    react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import NavBar from './components/Navbar/navbar'
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/Navbar/navbar';
+import Home from './components/Home/home';
+import Register from './components/Register/register';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Grid container spacing={3}>
-        <Grid item md={6}>
-          <h1>Aud-Eng</h1>
-          <h3>Find your solution</h3>
-        </Grid>
-        <Grid item md={6}>
-
-        </Grid>
-      </Grid>
+      <Switch>
+        <Route exact path='/' render={() => <Home/>}/>
+        <Route exact path='/register' render={() => <Register/>}/>
+      </Switch>
     </div>
   );
 }
